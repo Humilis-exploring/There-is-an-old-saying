65d452f4db897a28864cc79e044b374dd6561e16
Humilis-exploring <2073747136@qq.com>
2025-10-04 21:04:07 +0800

    提交

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="globalHeader">
+    <a-menu v-model:selectedKeys="current" mode="horizontal" :items="items" />
+  </div>
+</template>
+<script lang="ts" setup>
+import { h, ref } from 'vue';
+import { MailOutlined, HomeOutlined, SettingOutlined } from '@ant-design/icons-vue';
+import { MenuProps } from 'ant-design-vue';
+const current = ref<string[]>(['mail']);
+const items = ref<MenuProps['items']>([
+  {
+    key: '/',
+    icon: () => h(HomeOutlined),
+    label: '主页l',
+    title: '主页t',
+  },
+  {
+    key: '/user/login',
+    icon: () => h(MailOutlined),
+    label: '登录l',
+    title: '登录t',
+  },
+  {
+    key: '/user/register',
+    icon: () => h(SettingOutlined),
+    label: '注册l',
+    title: '注册t',
+    children: [
+      {
+        type: 'group',
+        label: 'Item 1',
+        children: [
+          {
+            label: 'Option 1',
+            key: 'setting:1',
+          },
+          {
+            label: 'Option 2',
+            key: 'setting:2',
+          },
+        ],
+      },
+      {
+        type: 'group',
+        label: 'Item 2',
+        children: [
+          {
+            label: 'Option 3',
+            key: 'setting:3',
+          },
+          {
+            label: 'Option 4',
+            key: 'setting:4',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    key: 'alipay',
+    label: h(
+      'a', 
+      { href: 'https://antdv.com', target: '_blank' }, '听说这是个跳转网页的链接'),
+    title: '听说这是个跳转网页的标题',
+  },
+]);
+</script>
+
---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="basicLayout">
+      <a-layout>
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://www.antdv.com" target="_blank" rel="noopener noreferrer">
+        Ant Design Vue
+        </a>
+        </a-layout-footer>
+      </a-layout>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import GlobalHeader from '@/components/GlobalHeader.vue';
+</script>
+<!-- <script lang="ts">
+export default {
+  // component options
+}
+</script> -->
+
+<style scoped>
+#basicLayout .footer{
+  background: #67e4c7;
+  text-align: center;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+#basicLayout .content{
+  margin-bottom: 20px;
+  padding: 20px; /* Prevent content from being hidden behind the fixed footer */
+  background: linear-gradient(to right, #49c4be, #ffffff);
+}
+#basicLayout .header{
+  background: white;
+  margin-bottom: 0px;
+  color: unset;
+  padding-inline: 20px;
+}
+</style>
